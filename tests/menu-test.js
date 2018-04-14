e2729829e83867505b5f6777373085c0fd2b620c
unknown <you@example.com>
2018-04-14 18:27:58 +0300

    better describe

--- tests/menu-test.js
+++ tests/menu-test.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 var HomePage = require('../pages/home.page'),
     MenuPage = require('../pages/menu.page');
 
-describe('Mocha Spec Sync example', function() {
+describe('Mocha Spec Sync first example', function() {
     it("verify Arcs entry in menu", function() {
         HomePage.click("Graphics");
         expect(MenuPage.arcsEntry.isVisible()).to.equal(true);
