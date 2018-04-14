34240f0ba6ee9adba33e222c22f2a0c610de5ba4
unknown <you@example.com>
2018-04-14 18:19:12 +0300

    setup for appium and test

---
+++ pages/home.page.js
@@ -0,0 +1,25 @@
+
+var Page = require('./page');
+
+var HomePage = Object.create(Page, {
+    graphicsTab: {
+        get: function () {
+            return browser.element(`~Graphics`);
+        }
+    },
+
+    click: {
+        value: function (tabName) {
+            if (tabName === "Graphics") {
+                this.graphicsTab.click();
+
+            } else {
+                throwError("Not implemented");
+            }
+
+        }
+    }
+
+});
+
+module.exports = HomePage;
